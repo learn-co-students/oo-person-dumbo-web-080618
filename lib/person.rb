ebd8781e5b5dbfe85d169f4b60d6ecfa5b785b5d
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-08-08 14:22:49 -0400

    classes

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+class Person
+  attr_accessor :name, :money, :happiness, :hygiene
+  def initialize(name,money,happiness,hygiene)
+    @name =name
+    @money = money
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+  def clean?
+    if @hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def happy?
+    if @happiness > 7
+      true
+    else
+      false
+    end
+  end
+end
